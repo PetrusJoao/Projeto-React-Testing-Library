63e433b9dffc5a909a81fc05f426560ef3db2143
João Pedro <joaopetrussousa@gmail.com>
2022-07-18 17:56:55 -0300

    Requisito 4 - Completo

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+test('Teste se a página contém um heading h2 com o texto "Page requested not found 😭"',
+  () => {
+    const { history } = renderWithRouter(<App />);
+
+    history.push('/pagina/que-nao-existe/');
+
+    const homeTitle = screen.getByText('Page requested not found');
+    expect(homeTitle).toBeInTheDocument();
+  });
+
+test('Teste se a página mostra a imagem "https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif"',
+  () => {
+    const { history } = renderWithRouter(<App />);
+
+    history.push('/pagina/que-nao-existe/');
+
+    const imgAlt = screen.getByAltText(
+      'Pikachu crying because the page requested was not found',
+    );
+    expect(imgAlt).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
